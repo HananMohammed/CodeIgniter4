521ee9c56602ac14970f9448c782e3a1a034b93d
HananMohammed <hananmohammed@github.com>
2021-07-22 09:23:36 -0400

    Enhance Code by Using Components,view_cell method and Libraries

---
+++ app/Views/components/post_item.php
@@ -0,0 +1,11 @@
+<div class="col-md-4 col-sm-12">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/assets/images/poster.jpg"  alt="post Picture">
+        <div class="card-body">
+            <h5 class="card-title"><?= $title ?></h5>
+            <p class="card-text">loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+
--- app/Views/single_post.php
+++ app/Views/single_post.php
@@ -1,11 +1,9 @@
 <h1><?= $title ?> </h1>
 <?php if (count($posts) > 0): ?>
-    <?php foreach ($posts as $post): ?>
-        <div>
-            <h3><?= $value ?></h3>
-            <img src="/assets/images/poster.jpg"  style="height: auto; width: 200px" alt="post Picture">
-            <p>loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text loreom Epsiom Text </p>
-        </div>
-    <?php endforeach; ?>
+    <div class="row">
+        <?php foreach ($posts as $post): ?>
+            <?= view_cell('\App\Libraries\Blog::postItem', ["title" => $post])?>
+        <?php endforeach; ?>
+    </div>
 <?php endif; ?>
 
